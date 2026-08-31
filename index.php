cacf938d22b8bf29fd1fcf29220f64d02e6f830e
Alfred Kimbork <kimborkalfred@gmail.com>
2026-08-31 09:55:33 +0200

    POST data

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
   echo "hello world";
+  $name = $_POST["name"] ?? "";
+?>
+<form action="" method="post">
+  <input type="text" name="name">
+  <input type="submit">
+</form>
+<?php echo $name ? "greetings " . $name : ""; ?>
